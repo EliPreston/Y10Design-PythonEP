fbb7db06f9c10fc4c4b66e7b26fac52f01c1b7ab
EliPreston <eli.preston@ucc.on.ca>
2019-09-24 13:47:59 -0400

    vending machine started in class

---
+++ classVendingMachine.py
@@ -0,0 +1,87 @@
+
+vm = [
+
+	{
+		"loca": "1",
+		"price": "1.50",
+		"item": "Lays Chips"
+	},
+	{
+		"loca": "2",
+		"price": "1.50",
+		"item": "Ketchup Chips"
+	},
+	{
+		"loca": "3",
+		"price": "1.50",
+		"item": "Ms. Vickeys"
+	},
+	{
+		"loca": "4",
+		"price": "2.50",
+		"item": "Mars Bar"
+	},
+	{
+		"loca": "5",
+		"price": "2.50",
+		"item": "KitKat"
+	},
+	{
+		"loca": "6",
+		"price": "2.50",
+		"item": "Coffee Crisp"
+	},
+	{
+		"loca": "7",
+		"price": "1.50",
+		"item": "Perrier"
+	},
+	{
+		"loca": "8",
+		"price": "1.75",
+		"item": "Chocolate Milk"
+	},
+	{
+		"loca": "9",
+		"price": "1.00",
+		"item": "Water"
+	}
+]
+
+
+
+for item in vm:
+	print(f"{item['loca']}: {item['item']} ...... {item['price']}")
+
+print("------------------")	
+choice = input("Which item would you like?\n")
+
+
+
+
+
+
+
+
+
+#if choice == '1' or '2':
+#	number = input("How many would you like?\n")
+
+#	if number == '2':
+#		print("3.00")
+
+
+
+#input("How many would you like?\n")
+
+
+
+
+
+
+
+
+
+
+
+
