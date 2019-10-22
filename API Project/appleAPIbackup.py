302771df0c210fe762f40b22ffd7d7db7689a858
EliPreston <eli.preston@ucc.on.ca>
2019-10-21 20:20:30 -0400

    API Project files, cleaned out useless or unused files from last push

--- API Project/appleAPIbackup.py
+++ API Project/appleAPIbackup.py
@@ -1,28 +1,70 @@
 # Eli Preston
 # Upper Canada College
-# This program displays the stock closing prices of APPLE from the past 4 years in a bar graph using Chart js
+# This program displays the stock closing prices of APPLE from 2014 to 2019 in a bar graph using Chart js.
 
 import requests
 import json
 import pprint
 
-# Prints JSON data in a more readable form ~ http://jsonprettyprint.com/
 
 
 def writeHTML(data, closingPrices, dates):
-    myfile = open("myAPI.html","w")
+    myfile = open("appleAPI.html","w")
     myfile.write("<h1>JSON file returned by API call</h1>")
     myfile.write("<p>Copy and paste to <a href='https://jsoneditoronline.org/'>JSON editor</a> for pretty format.</p>")
 
-    # newColor2015 = []
-    # color2015 = dates[:53]
-    # print(color2015)
+    bgColors = []
+    for date in dates:
+        if date[:4] == "2014":
+            bgColors.append("'rgba(0, 255, 0, 1)'")
+        elif date[:4] == "2015":
+            bgColors.append("'rgba(255, 255, 0, 1)'")
+        elif date[:4] == "2016":
+            bgColors.append("'rgba(255, 0, 0, 1)'")
+        elif date[:4] == "2017":
+            bgColors.append("'rgba(0, 0, 255, 1)'")
+        elif date[:4] == "2018":
+            bgColors.append("'rgba(255, 0, 255, 1)'")
+        elif date[:4] == "2019":
+            bgColors.append("'rgba(0, 255, 255, 1)'")
 
-    # for i in color2015:
-    #     print(i)
-    #     newColor2015.append("'rgba(255, 0, 0, 1)'")
-    # print(newColor2015)
 
+
+    fourteen = []
+    fifteen = []
+    sixteen = []
+    seventeen = []
+    eighteen = []
+    nineteen = []
+
+    index = 0
+    for yr in dates:
+        if yr[:4] == "2014":
+            print('2014')
+            fourteen.append(closingPrices[index])
+        elif yr[:4] == "2015":
+            print('2015')
+            fifteen.append(closingPrices[index])
+        elif yr[:4] == "2016":
+            print('2016')
+            sixteen.append(closingPrices[index])
+        elif yr[:4] == "2017":
+            print('2017')
+            seventeen.append(closingPrices[index])
+        elif yr[:4] == "2018":
+            print('2018')
+            eighteen.append(closingPrices[index])
+        elif yr[:4] == "2019":
+            print('2019')
+            nineteen.append(closingPrices[index])
+        index += 1
+    
+    print(fourteen)
+    print(fifteen)
+    print(sixteen)
+    print(seventeen)
+    print(eighteen)
+    print(nineteen)
 
 
     myfile.write("""
@@ -37,11 +79,10 @@ def writeHTML(data, closingPrices, dates):
             data: {
                 labels: """ + "".join(str(dates)) + """,
                 datasets: [{
-                    label: 'Closing Prices',
+                    label: 'Closing Prices of Apple From 2014 - Present',
                     data: """ + "".join(str(closingPrices)) + """,
-                    backgroundColor: [],
+                    backgroundColor: [""" + ",".join(bgColors) + """],
                     borderColor: [
-                        'rgba(255, 99, 132, 1)',
                     ],
                     borderWidth: 1
                 }]
@@ -81,12 +122,10 @@ def main():
       
         data = response.content
         data_as_str = data.decode()
-        # writeHTML(data_as_str)
         datajson = response.json()
         
         dataSymbol = datajson['symbol']
         dataPoints = datajson['historical']
-        #pprint.pprint(datajson)
         print(dataSymbol)
 
 
@@ -95,21 +134,11 @@ def main():
         dates = []
 
         for point in dataPoints:
-            #print(f"{point['ticker']}: \n\tOpen Price; {point['open']} \n\tChange; {point['changes']} \n\tDate; {point['date']}  \n")
             print(f"Date: {point['label']} \n\tOpening Price: {point['open']} \n\tClosing Price: {point['close']} \n\tChange: {point['change']} \n\n")
             closingPrices.append(point["close"])
             dates.append(point["date"])
             
         writeHTML(data_as_str, closingPrices, dates)
-           
-        """
-            #writeHTML(variable)
-        myfile = open("myAPI.html","w")
-	    myfile.write("<p>Copy and paste to <a href='https://jsoneditoronline.org/'>JSON editor</a> for pretty format.</p>")
-	    myfile.write(data)
-	    myfile.close()
-        """
-        #writeHTML(variable)
 
     else:
         data = "Error has occured"
